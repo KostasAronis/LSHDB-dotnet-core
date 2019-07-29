32979e6a5ece5f83405269623312fa7a3b44f182
KostasAronis <aronis_k@yahoo.com>
2019-07-29 21:28:56 +0300

    correct structure and helper files

--- src/standalone/src/Program.cs
+++ src/standalone/src/Program.cs
@@ -6,7 +6,7 @@ namespace standalone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ASDF");
         }
     }
 }
