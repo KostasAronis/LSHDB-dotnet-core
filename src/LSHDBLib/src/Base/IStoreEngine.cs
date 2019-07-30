6f11473f129ba9e95a2a0e03b8bd523318b6e1f2
KostasAronis <aronis_k@yahoo.com>
2019-07-30 20:24:54 +0300

    store engine correct interface

---
+++ src/LSHDBLib/src/Base/IStoreEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace LSHDBLib.Base
+{
+    public enum Engine{
+
+    }
+    public interface IStoreEngine {
+    
+        void set(String key, Object data);
+        Object get(String key);
+        bool contains(String key);
+        long count();
+        void close();
+        IEnumerable createIterator();
+    }
+}
